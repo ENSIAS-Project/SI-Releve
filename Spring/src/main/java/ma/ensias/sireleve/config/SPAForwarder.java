6687ff2f0c7285788d84eaf63e19dc4935ecbb2e
EL BOUZIYANI Anas <aaelbouziyani@gmail.com>
2025-12-17 11:36:00 +0100

    fix for SPA forwarding

---
+++ Spring/src/main/java/ma/ensias/sireleve/config/SPAForwarder.java
@@ -0,0 +1,14 @@
+package ma.ensias.sireleve.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SPAForwarder {
+
+	@RequestMapping(value = "/{path:[^\\.]*}")
+	public String redirect(@PathVariable String path) {
+		return "forward:/";
+	}
+}
