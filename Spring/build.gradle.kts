c8479b6ee5f47a7cb85b4ef570f4c4a1c8432a34
EL BOUZIYANI Anas <aaelbouziyani@gmail.com>
2025-12-18 20:20:04 +0100

    add mail capabilites with mailhog on localhost

--- Spring/build.gradle.kts
+++ Spring/build.gradle.kts
@@ -30,6 +30,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-mail")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
 	implementation("com.google.zxing:core:3.3.0")
     implementation("com.google.zxing:javase:3.3.0")
 	compileOnly("org.projectlombok:lombok")
